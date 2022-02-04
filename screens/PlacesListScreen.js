45cb617a55ec2db3a30b8521f02b6f4dd7e9b0c1
vladotoka <vladimir.k.terziev@tutanota.com>
2022-02-04 16:03:33 +0200

    15 fetch data from DB

--- screens/PlacesListScreen.js
+++ screens/PlacesListScreen.js
@@ -1,12 +1,15 @@
 import { FlatList, StyleSheet, Text, View, Platform } from 'react-native';
-import React, { useLayoutEffect } from 'react';
+import React, { useLayoutEffect, useEffect } from 'react';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+
+import * as placesActions from '../store/places-actions';
 
 import HeaderButton from '../components/HeaderButton';
 import PlaceItem from '../components/PlaceItem';
 
 const PlacesListScreen = (props) => {
+  const dispatch = useDispatch();
   const places = useSelector((state) => state.places.places);
   console.log(places[0]);
 
@@ -24,13 +27,17 @@ const PlacesListScreen = (props) => {
     });
   }, [props.navigaton]);
 
+  useEffect(() => {
+    dispatch(placesActions.loadPlaces());
+  }, [dispatch]);
+
   return (
     <FlatList
       data={places}
       renderItem={(itemData) => (
         <PlaceItem
           title={itemData.item.title}
-          address='адрес'
+          address="адрес"
           image={itemData.item.imageUri}
           onSelect={() => {
             props.navigation.navigate('PlaceDetail', {
--- store/places-actions.js
+++ store/places-actions.js
@@ -1,7 +1,9 @@
 import * as FileSystem from 'expo-file-system';
-import { insertPlace } from '../helpers/db';
+
+import { insertPlace, fetchPlaces } from '../helpers/db';
 
 export const ADD_PLACE = 'ADD_PLACE';
+export const SET_PLACES = 'SET_PLACES';
 
 export const addPlace = (title, image) => {
   return async (dispatch) => {
@@ -13,13 +15,27 @@ export const addPlace = (title, image) => {
         from: image,
         to: newPath,
       });
-      const dbResult = await insertPlace(title, image, 'адрес', 42.24, 24.42);
+      const dbResult = await insertPlace(title, newPath, 'адрес', 42.24, 24.42);
       console.log(dbResult);
 
       dispatch({
         type: ADD_PLACE,
         placeData: { id: dbResult.insertId, title: title, image: newPath },
       });
+    } catch (err) {
+      console.log(err);
+      throw err;
+    }
+  };
+};
+
+export const loadPlaces = () => {
+  return async (dispatch) => {
+    try {
+      const dbResult = await fetchPlaces();
+      console.log('dbResult:');
+      console.log(dbResult);
+      dispatch({ type: SET_PLACES, places: dbResult.rows._array });
     } catch (err) {
       console.log(err);
       throw err;
