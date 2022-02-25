eda1170a29f4fd193e894565899ace888ca179a8
vladotoka <vladimir.k.terziev@tutanota.com>
2022-02-25 14:53:37 +0200

    minor

--- navigation/PlacesNavigator.js
+++ navigation/PlacesNavigator.js
@@ -11,30 +11,39 @@ import PlacesListScreen from '../screens/PlacesListScreen';
 const Stack = createStackNavigator();
 
 const MainNavigator = () => {
-  return (
-    <Stack.Navigator
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: Platform.OS === 'android' ? Colors.primary : '',
-        },
-        headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary,
-      }}
-    >
-      <Stack.Screen
-        name="List" 
-        component={PlacesListScreen}
-        options={{ title: 'Всички места' }}
-      />
-      <Stack.Screen name="PlaceDetail" component={PlaceDetailScreen} options={{ title: 'Подробни данни' }}/>
-      <Stack.Screen
-        name="NewPlace"
-        component={NewPlaceScreen}
-        options={{ title: 'Ново място' }}
-        initialParams={{ mapPickedLocation: null }}
-      />
-      <Stack.Screen name="Map" component={MapScreen}  options={{ title: 'Карта' }} initialParams={{ readonly: null, initialLocation: null }}/>
-    </Stack.Navigator>
-  );
+	return (
+		<Stack.Navigator
+			screenOptions={{
+				headerStyle: {
+					backgroundColor: Platform.OS === 'android' ? Colors.primary : '',
+				},
+				headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary,
+			}}
+		>
+			<Stack.Screen
+				name="List"
+				component={PlacesListScreen}
+				options={{ title: 'Всички места' }}
+			/>
+			<Stack.Screen
+				name="PlaceDetail"
+				component={PlaceDetailScreen}
+				options={{ title: 'Подробни данни' }}
+			/>
+			<Stack.Screen
+				name="NewPlace"
+				component={NewPlaceScreen}
+				options={{ title: 'Ново място' }}
+				initialParams={{ mapPickedLocation: null }}
+			/>
+			<Stack.Screen
+				name="Map"
+				component={MapScreen}
+				options={{ title: 'Карта' }}
+				initialParams={{ readonly: null, initialLocation: null }}
+			/>
+		</Stack.Navigator>
+	);
 };
 
 export default MainNavigator;
